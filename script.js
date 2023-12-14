488f7bd4629fb706b4f04c2a4b146ee42c51951d
soli2060 <144271193+soli2060@users.noreply.github.com>
2023-12-14 01:51:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function renderTasks() {
     taskList.innerHTML = '';
     tasks.forEach((task, index) => {
         const taskDiv = document.createElement('div');
-        taskDiv.className = task.completed ? 'task-completed' : 'task-item';
+        taskDiv.className = task.completed ? 'task-completed' : '';
         taskDiv.innerHTML = `
-            <span>${task.description} - Due: ${task.dueDate}</span>
-            <button onclick="toggleComplete(${index})">${task.completed ? 'Mark Incomplete' : 'Complete'}</button>
+            ${task.description}
+            <button onclick="toggleComplete(${index})">${task.completed ? 'Unmark' : 'Complete'}</button>
             <button onclick="deleteTask(${index})">Delete</button>
         `;
         taskList.appendChild(taskDiv);
@@ -22,20 +22,11 @@ function renderTasks() {
 
 function addTask() {
     const taskInput = document.getElementById('taskInput');
-    const taskDueDate = document.getElementById('taskDueDate');
-    const newTask = {
-        description: taskInput.value.trim(),
-        dueDate: taskDueDate.value,
-        completed: false
-    };
-    if (newTask.description && newTask.dueDate) {
-        tasks.push(newTask);
+    const newTask = taskInput.value.trim();
+    if (newTask) {
+        tasks.push({ description: newTask, completed: false });
         saveTasks();
-        checkDueTasks();
         taskInput.value = '';
-        taskDueDate.value = '';
-    } else {
-        alert('Please enter a task and due date');
     }
 }
 
@@ -49,28 +40,4 @@ function deleteTask(index) {
     saveTasks();
 }
 
-function checkDueTasks() {
-    const today = new Date().toISOString().split('T')[0];
-    let reminders = tasks.filter(task => !task.completed && task.dueDate === today);
-    if (reminders.length > 0) {
-        let reminderText = reminders.map(task => `Task '${task.description}' is due today!`).join('\n');
-        showModal(reminderText);
-    }
-}
-
-function showModal(text) {
-    const modal = document.getElementById('modal');
-    const modalText = document.getElementById('modalText');
-    modalText.innerText = text;
-    modal.style.display = 'block';
-}
-
-function closeModal() {
-    const modal = document.getElementById('modal');
-    modal.style.display = 'none';
-}
-
-window.onload = () => {
-    renderTasks();
-    checkDueTasks(); // Check for reminders on page load
-};
+window.onload = renderTasks;
