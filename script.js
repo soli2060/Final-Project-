1f6333217c4c49663fb1cf2c216fe478d11a19b7
soli2060 <144271193+soli2060@users.noreply.github.com>
2023-12-14 00:31:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,76 +3,100 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Your Website</title>
-  <link rel="stylesheet" href="styles.css">
-  <link rel="icon" href="logo.png" type="image/x-icon">
+  <title>Your Tasks</title>
   <style>
-    /* Your existing styles */
+    /* Your existing styles here */
+
+    /* Style for the modal */
+    .modal-container {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: rgba(0, 0, 0, 0.5);
+    }
+
+    .modal-content {
+      background-color: white;
+      padding: 20px;
+      border-radius: 5px;
+      text-align: center;
+    }
   </style>
 </head>
 <body>
-  <nav>
-    <div class="nav-container">
-      <a href="index.html" style="color: white;">Go Back to Home Page</a>
-      <a href="page7.html" style="color: white;">Page 7</a>
-    </div>
-  </nav>
-  <div class="container">
-    <main>
-      <h2>Welcome to ShubhaMegh LLC</h2>
-      <p>At ShubhaMegh LLC, we are dedicated to providing you with valuable guidance and suggestions to help you achieve your future goals. With a commitment to excellence and a passion for your success, we are your trusted partner on the journey towards realizing your dreams.</p>
-        
-      <p>Our experienced team is here to offer tailored solutions and expert advice that empower you to make informed decisions for a brighter tomorrow. Whether you're striving for personal growth, professional success, or business expansion, we're here to support you every step of the way.</p>
-        
-      <p>Thank you for choosing ShubhaMegh LLC as your partner in shaping a successful future. Together, we'll navigate the path to your aspirations and bring your goals within reach.</p>
-    </main>
-  </div>
-  <footer>
-    <p>&copy; ShubhaMegh LLC 2023</p>
-  </footer>
+  <!-- Your existing HTML structure here -->
 
+  <!-- Add this script section at the end of your body tag -->
   <script>
     let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
     function saveTasks() {
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-        renderTasks();
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+      renderTasks();
     }
 
     function renderTasks() {
-        // Your existing renderTasks function
+      // Your existing renderTasks function
     }
 
     function showCustomModal(message) {
-        const modalContainer = document.createElement('div');
-        modalContainer.classList.add('modal-container');
-        modalContainer.setAttribute('id', 'reminder-modal');
+      const modalContainer = document.createElement('div');
+      modalContainer.classList.add('modal-container');
+      modalContainer.setAttribute('id', 'reminder-modal');
 
-        const modalContent = document.createElement('div');
-        modalContent.classList.add('modal-content');
-        modalContent.innerHTML = `
-            <p>${message}</p>
-            <button onclick="removeReminder()">Okay</button>
-        `;
+      const modalContent = document.createElement('div');
+      modalContent.classList.add('modal-content');
+      modalContent.innerHTML = `
+        <p>${message}</p>
+        <button onclick="removeReminder()">Okay</button>
+      `;
 
-        modalContainer.appendChild(modalContent);
-        document.body.appendChild(modalContainer);
+      modalContainer.appendChild(modalContent);
+      document.body.appendChild(modalContainer);
     }
 
     function removeReminder() {
-        const modalContainer = document.getElementById('reminder-modal');
-        if (modalContainer) {
-            modalContainer.remove();
-        }
+      const modalContainer = document.getElementById('reminder-modal');
+      if (modalContainer) {
+        modalContainer.remove();
+      }
     }
 
     function addTask() {
-        // Your updated addTask function
+      const taskInput = document.getElementById('task-input');
+      const dueDateInput = document.getElementById('task-due-date');
+
+      const task = taskInput.value.trim();
+      const dueDate = dueDateInput.value;
+
+      if (task && dueDate) {
+        tasks.push({ title: task, dueDate, completed: false });
+        taskInput.value = '';
+        dueDateInput.value = '';
+        saveTasks();
+
+        // Trigger the reminder
+        const daysRemaining = calculateDaysRemaining(dueDate);
+        const reminderMessage = `New task added: ${task} is due in ${daysRemaining} days!`;
+
+        // Display a custom modal with the calculated days remaining
+        showCustomModal(reminderMessage);
+
+        // Reload the page to display the updated task list and the new reminder
+        renderTasks();
+      } else {
+        alert('Please enter both a task and a due date');
+      }
     }
 
-    // Your existing toggleComplete, deleteTask, calculateDaysRemaining, findNearestTask, and window.onload functions
-    // ...
+    // Your existing functions here
 
+    // Call renderTasks when the page loads
     window.onload = renderTasks;
   </script>
 </body>
