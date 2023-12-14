206ca8fb8b9863e1a8ff565ed2898b86b7450c80
soli2060 <144271193+soli2060@users.noreply.github.com>
2023-12-14 01:02:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Your Website</title>
-  <style>
-    /* Your existing styles here */
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-    /* Additional styles for the reminder modal */
-    .modal-container {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background: rgba(0, 0, 0, 0.5);
+function saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    renderTasks();
+}
+
+function renderTasks() {
+    const taskList = document.getElementById('taskList');
+    taskList.innerHTML = ''; // Clear existing tasks
+    tasks.forEach((task, index) => {
+        const taskDiv = document.createElement('div');
+        taskDiv.textContent = task;
+        // Additional functionalities like delete or toggle can be added here
+        taskList.appendChild(taskDiv);
+    });
+}
+
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const newTask = taskInput.value.trim();
+    if (newTask) {
+        tasks.push(newTask);
+        saveTasks();
+        taskInput.value = ''; // Clear the input field after adding the task
+    } else {
+        showCustomModal('Please enter a task');
     }
+}
 
-    .modal-content {
-      background: white;
-      padding: 20px;
-      border-radius: 5px;
-      text-align: center;
-    }
-  </style>
-</head>
-<body>
-  <!-- Your existing HTML content here -->
+function showCustomModal(message) {
+    const modalContainer = document.createElement('div');
+    modalContainer.classList.add('modal-container');
 
-  <script>
-    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-
-    function saveTasks() {
-      localStorage.setItem('tasks', JSON.stringify(tasks));
-      renderTasks();
-    }
-
-    function renderTasks() {
-      // Your existing renderTasks function
-    }
-
-    function showCustomModal(message) {
-      const modalContainer = document.createElement('div');
-      modalContainer.classList.add('modal-container');
-
-      const modalContent = document.createElement('div');
-      modalContent.classList.add('modal-content');
-      modalContent.innerHTML = `
+    const modalContent = document.createElement('div');
+    modalContent.classList.add('modal-content');
+    modalContent.innerHTML = `
         <p>${message}</p>
         <button onclick="removeReminder()">Okay</button>
-      `;
+    `;
 
-      modalContainer.appendChild(modalContent);
-      document.body.appendChild(modalContainer);
-    }
+    modalContainer.appendChild(modalContent);
+    document.body.appendChild(modalContainer);
+}
 
-    function removeReminder() {
-      const modalContainer = document.querySelector('.modal-container');
-      if (modalContainer) {
+function removeReminder() {
+    const modalContainer = document.querySelector('.modal-container');
+    if (modalContainer) {
         modalContainer.remove();
-      }
     }
+}
 
-    function addTask() {
-      // Your updated addTask function
-    }
+// Other functions (toggleComplete, deleteTask, calculateDaysRemaining, findNearestTask) go here
 
-    // Other functions (toggleComplete, deleteTask, calculateDaysRemaining, findNearestTask) go here
-
-    // Call renderTasks when the page loads
-    window.onload = renderTasks;
-  </script>
-</body>
-</html>
+window.onload = renderTasks;
